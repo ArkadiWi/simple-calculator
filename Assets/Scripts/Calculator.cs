811b1b017b7a812f1e64274dcfbd13730d613cd9
Arkadiusz Wieczorkowski <arkadi.wi@gmail.com>
2022-10-01 09:32:01 +0200

    Change public variables to private

--- Assets/Scripts/Calculator.cs
+++ Assets/Scripts/Calculator.cs
@@ -1,44 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using System;
-using System.Runtime.CompilerServices;
-using UnityEditor.PackageManager.Requests;
-using UnityEditorInternal;
+
 
 public class Calculator : MonoBehaviour
 {
     
-    private string _tempNumbers;
-    private int _counter;
+    private string collectNumbersInString;
+    private int countPuttedNumbers;
+    private float resultOfCalculation;
+    private string resultAfterTappedEqual;
 
-    public float result;
-    public string finalResult;
-    public string tempResult1;
-    public string tempResult2;
-    public string operation1;
-    public string operation2;
-    public float number1;
-    public float number2;
+    private string firstStringOfNumbers;
+    private string secStringOfNumbers;
+    private string firstSelectMathOperation;
+    private string secSelectMathOperation;    
+    private TMP_InputField inputField;
 
-    public TMP_InputField InputField;
-
-   
 
     // Start is called before the first frame update
     void Start()
     {
-        FullReset();        
+        inputField = FindObjectOfType<TMP_InputField>();        
+        FullReset();
     }
 
     public void ClickNumber (int val)
     {
-        if (_counter < 10)
+        if (countPuttedNumbers < 10)
         {
-            _counter++;           
-            _tempNumbers += val.ToString();
-            ShowOnScreen(_tempNumbers);
+            countPuttedNumbers++;           
+            collectNumbersInString += val.ToString();
+            ShowOnScreen(collectNumbersInString);
         }        
             
     }
@@ -46,32 +38,30 @@ public class Calculator : MonoBehaviour
 
     public void ClickOperation(string operation)
     {
-        if (tempResult1 == null && _tempNumbers != null)
+        if (firstStringOfNumbers == "" && collectNumbersInString != "")
         {
-            Debug.Log("Jestem w 1szym warunku");
-            tempResult1 = _tempNumbers;
-            operation1 = operation;
-            Debug.Log(tempResult1);
+            firstStringOfNumbers = collectNumbersInString;
+            firstSelectMathOperation = operation;            
             Reset();
-            ShowOnScreen(tempResult1);
+            ShowOnScreen(firstStringOfNumbers);
         }
-        else if (_tempNumbers != null)
+        else if (collectNumbersInString != "")
         {
-            Debug.Log("Jestem w 2gim warunku");
-            tempResult2 = _tempNumbers;
-            operation2 = operation;
+            secStringOfNumbers = collectNumbersInString;
+            secSelectMathOperation = operation;
 
-            result = DoCalculation(tempResult1, tempResult2, operation1);
+
+
+            resultOfCalculation = DoCalculation(firstStringOfNumbers, secStringOfNumbers, firstSelectMathOperation);
+            resultAfterTappedEqual = resultOfCalculation.ToString();
+            ShowOnScreen(resultAfterTappedEqual);
             Reset();
-            tempResult1 = result.ToString();
-            finalResult = tempResult1;
-            ShowOnScreen(tempResult1);
 
-            tempResult2 = null;
+            secStringOfNumbers = "";
             operation = null;
-            operation1 = operation2;
+            firstSelectMathOperation = secSelectMathOperation;
 
-            result = 0f;
+            resultOfCalculation = 0f;
         }
         else
         {           
@@ -83,75 +73,75 @@ public class Calculator : MonoBehaviour
     public void ClickEqual()
     {        
         FullReset();
-        ShowOnScreen(finalResult);
-        //finalResult = null;
+        ShowOnScreen(resultAfterTappedEqual);
+        collectNumbersInString = resultAfterTappedEqual;
     }
 
     public void ClickPeriod(string val)
     {
-        if(_tempNumbers == null)
+        if(collectNumbersInString == "")
         {
-            _tempNumbers = "0";            
+            collectNumbersInString = "0";            
         }                    
         
-        if(!_tempNumbers.Contains(","))
+        if(!collectNumbersInString.Contains(","))
         {
-            _tempNumbers += ",";
+            collectNumbersInString += ",";
         }
 
-        ShowOnScreen(_tempNumbers);
+        ShowOnScreen(collectNumbersInString);
     }        
     
     private void ShowOnScreen(string result)
     {
-        InputField.text = null;
-        InputField.text = result;
+        inputField.text = "";
+        inputField.text = result;
     }
 
     private void Reset()
     {
-        InputField.text = "0";
-        _counter = 0;
-        _tempNumbers = null;        
+        inputField.text = "0";
+        countPuttedNumbers = 0;
+        collectNumbersInString = "";        
     }
 
-    public void FullReset()
+    private void FullReset()
     {
-        tempResult1 = null;
-        tempResult2 = null;        
-        operation1 = null;
-        operation2 = null;
-        InputField.text = "0";
-        _counter = 0;
-        _tempNumbers = null;        
+        firstStringOfNumbers = "";
+        secStringOfNumbers = "";        
+        firstSelectMathOperation = "";
+        secSelectMathOperation = "";
+        inputField.text = "0";
+        countPuttedNumbers = 0;
+        collectNumbersInString = "";        
     }
 
 
-    private float DoCalculation(string val1, string val2, string operation1)
+    private float DoCalculation(string val1, string val2, string operation)
     {
-        number1 = float.Parse(val1);
-        number2 = float.Parse(val2);
+        float number1 = float.Parse(val1);
+        float number2 = float.Parse(val2);
 
-        switch (operation1)
+        switch (operation)
         {
             case ("+"):
-                result = number1 + number2;
+                resultOfCalculation = number1 + number2;
                 break;
 
             case ("-"):
-                result = number1 - number2;
+                resultOfCalculation = number1 - number2;
                 break;
 
             case ("*"):
-                result = number1 * number2;
+                resultOfCalculation = number1 * number2;
                 break;
 
             case ("/"):
-                result = number1 / number2;
+                resultOfCalculation = number1 / number2;
                 break;
         }
 
-        return result;
+        return resultOfCalculation;
     }
 
 
